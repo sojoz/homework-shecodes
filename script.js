aa81c5b9b70ebb8a3b77f8a8bc3a76c8325664db
Joz-byte <josephinezelleke@gmail.com>
2023-05-25 18:26:31 +0200

    added css and javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+function showTime(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+
+
+let choice = document.querySelector("#timezones");
+choice.addEventListener("change", showTime);
